a71d0a33cf2b19320dd708058976cd619c7b659f
Oussama ELMOUNKAD <oelmounkad@gmail.com>
2022-05-23 21:14:44 +0100

    React scaffolder component

---
+++ src/components/react-scaffolder/ReactScaffolderComponent.tsx
@@ -0,0 +1,9 @@
+import './ReactScaffolderComponent.css';
+
+const ReactScaffolderComponent = () => {
+  return (
+    <div>ReactScaffolderComponent</div>
+  )
+}
+
+export default ReactScaffolderComponent

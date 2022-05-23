c04f28b268c7c0a0db7a5ef8ebce83fdc201cf5a
Oussama ELMOUNKAD <oelmounkad@gmail.com>
2022-05-23 21:15:21 +0100

    scaffolder wrapper component

---
+++ src/components/scaffolder/ScaffolderComponent.tsx
@@ -0,0 +1,21 @@
+import { useContext } from 'react';
+import AppContext from '../../context/AppContext/AppContext';
+import AngularScaffolderComponent from '../angular-scaffolder/AngularScaffolderComponent';
+import ReactScaffolderComponent from '../react-scaffolder/ReactScaffolderComponent';
+import './ScaffolderComponent.css';
+
+const ScaffolderComponent = () => {
+
+  const appContext = useContext(AppContext);
+  const { chosenScaffolder } = appContext;
+
+  return (
+    <>
+    { chosenScaffolder === 'angular' ? <AngularScaffolderComponent /> :
+    <ReactScaffolderComponent />
+    } 
+    </>
+  )
+}
+
+export default ScaffolderComponent

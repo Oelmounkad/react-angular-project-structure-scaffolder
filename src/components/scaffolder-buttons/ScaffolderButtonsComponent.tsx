e1bd0e41a6e0f3628f917e56ded9d200380027b2
Oussama ELMOUNKAD <oelmounkad@gmail.com>
2022-05-23 21:15:10 +0100

    scaffolder buttons component

---
+++ src/components/scaffolder-buttons/ScaffolderButtonsComponent.tsx
@@ -0,0 +1,31 @@
+import { Button, Stack } from "@chakra-ui/react";
+import { useContext } from "react";
+import { DiAngularSimple, DiReact } from "react-icons/di";
+import AppContext from "../../context/AppContext/AppContext";
+import "./ScaffolderButtonsComponent.css";
+const ScaffolderButtonsComponent = () => {
+  const appContext = useContext(AppContext);
+  const { updateScaffolder } = appContext;
+
+  return (
+      <>
+    <div className="switch-buttons">
+      <Stack direction="row" spacing={4}>
+        <Button
+          leftIcon={<DiAngularSimple />}
+          colorScheme="pink"
+          variant="solid"
+          onClick={() => updateScaffolder('angular')}
+        >
+          Angular Scaffolder
+        </Button>
+        <Button leftIcon={<DiReact />} colorScheme="blue" variant="solid" onClick={() => updateScaffolder('react')}>
+          React Scaffolder
+        </Button>
+      </Stack>
+    </div>
+      </>
+  );
+};
+
+export default ScaffolderButtonsComponent;

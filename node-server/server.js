02a2e0672a71eb5686ac6454937d703ab837ccb6
Oussama ELMOUNKAD <oelmounkad@gmail.com>
2022-06-23 10:20:27 +0100

    cors middleware undo

--- node-server/server.js
+++ node-server/server.js
@@ -29,24 +29,7 @@ app.get('/', (req,res) => {
   res.send('Hello world');
 })
 
-app.post("/angular-project-scaffolder", (req, res, next) => {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-} , (req, res) => {
+app.post("/angular-project-scaffolder", (req, res) => {
   const projectStructure = JSON.parse(req.body.projectStructure);
   const randomId = v4();
   const src = `src-${randomId}`;

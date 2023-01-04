4c137dfe5b1a861ffa4151f48ed252766800b8fb
Oussama ELMOUNKAD <oelmounkad@gmail.com>
2023-01-04 16:49:54 +0100

    changed url

--- src/components/angular-scaffolder/AngularScaffolderComponent.tsx
+++ src/components/angular-scaffolder/AngularScaffolderComponent.tsx
@@ -23,20 +23,34 @@ import AngularModuleComponent from "../angular module/AngularModuleComponent";
 import { v4 as uuidv4 } from "uuid";
 import "./AngularScaffolderComponent.css";
 import { FaAngular, FaDownload } from "react-icons/fa";
-import axios from 'axios';
+import axios from "axios";
 import fileDownload from "js-file-download";
 
 const AngularScaffolderComponent = () => {
-  const { isOpen : isOpenAddModule, onOpen: onOpenAddModule, onClose: onCloseAddModule } = useDisclosure();
-  const { isOpen : isOpenAddGlobalService, onOpen: onOpenAddGlobalService, onClose: onCloseAddGlobalService } = useDisclosure();
+  const {
+    isOpen: isOpenAddModule,
+    onOpen: onOpenAddModule,
+    onClose: onCloseAddModule,
+  } = useDisclosure();
+  const {
+    isOpen: isOpenAddGlobalService,
+    onOpen: onOpenAddGlobalService,
+    onClose: onCloseAddGlobalService,
+  } = useDisclosure();
 
   const [moduleName, setModuleName] = useState("");
 
   const [globalServiceName, setGlobalServiceName] = useState("");
 
   const [loading, setLoading] = useState(false);
-  
-  const { addModule, addGlobalService, modules, globalServices, removeGlobalService } = useStore();
+
+  const {
+    addModule,
+    addGlobalService,
+    modules,
+    globalServices,
+    removeGlobalService,
+  } = useStore();
 
   const toast = useToast();
 
@@ -51,12 +65,12 @@ const AngularScaffolderComponent = () => {
       });
       return;
     }
-    const newModule : IModule = {
+    const newModule: IModule = {
       id: uuidv4(),
-      name: `${moduleName.charAt(0).toUpperCase()}${moduleName.slice(1)}Module`
-    }
+      name: `${moduleName.charAt(0).toUpperCase()}${moduleName.slice(1)}Module`,
+    };
     addModule(newModule);
-    setModuleName('');
+    setModuleName("");
     onCloseAddModule();
   };
 
@@ -71,12 +85,14 @@ const AngularScaffolderComponent = () => {
       });
       return;
     }
-    const newGlobalService : IService = {
+    const newGlobalService: IService = {
       id: uuidv4(),
-      name: `${globalServiceName.charAt(0).toUpperCase()}${globalServiceName.slice(1)}Service`
-    }
+      name: `${globalServiceName
+        .charAt(0)
+        .toUpperCase()}${globalServiceName.slice(1)}Service`,
+    };
     addGlobalService(newGlobalService);
-    setGlobalServiceName('');
+    setGlobalServiceName("");
     onCloseAddGlobalService();
   };
 
@@ -84,90 +100,120 @@ const AngularScaffolderComponent = () => {
     setLoading(true);
     const payload = {
       modules,
-      globalServices
+      globalServices,
+    };
+
+    axios({
+      url: "https://angular-react-scaffolder-node-api.onrender.com/angular-project-scaffolder",
+      method: "POST",
+      responseType: "arraybuffer",
+      data: {
+        projectStructure: JSON.stringify(payload),
+      },
+    })
+      .then((res) => {
+        setLoading(false);
+        fileDownload(res.data, "angular-project-structure.zip");
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   };
 
-  axios({
-    url : 'https://angular-scaffolder-node.herokuapp.com/angular-project-scaffolder',
-    method: 'POST',
-    responseType: 'arraybuffer',
-  data: {
-    projectStructure: JSON.stringify(payload)
-  }
-  }).then((res) => {
-    setLoading(false);
-    fileDownload(res.data, 'angular-project-structure.zip');
-  }).finally(() => {
-    setLoading(false);
-  })
-  };
-
-  const isGenerateButtonEnabled = () : boolean => {
+  const isGenerateButtonEnabled = (): boolean => {
     return modules.length > 0 || globalServices.length > 0;
-  }
+  };
 
   return (
     <div className="angular-scaffolder-wrapper">
-      <div className="tools"> <i> <u>Tools:</u> </i> </div>
-      <Stack direction='row' spacing={4} align='center' justifyContent='center'>
-          <Button onClick={onOpenAddModule} leftIcon={<AddIcon />} colorScheme='red'>
-            Add Module
-          </Button>
-          <Button onClick={onOpenAddGlobalService} leftIcon={<AddIcon />} colorScheme='yellow'>
-            Add Global Service
-          </Button>
-          <Button onClick={() => generateAngularProject()} leftIcon={<FaDownload />} colorScheme='green' disabled={!isGenerateButtonEnabled()}>
-            Generate Angular Project
-          </Button>
-         {loading && <Spinner
-            thickness='4px'
-            speed='0.65s'
-            emptyColor='gray.200'
-            color='green.500'
-            size='lg'
-          />  } 
+      <div className="tools">
+        {" "}
+        <i>
+          {" "}
+          <u>Tools:</u>{" "}
+        </i>{" "}
+      </div>
+      <Stack direction="row" spacing={4} align="center" justifyContent="center">
+        <Button
+          onClick={onOpenAddModule}
+          leftIcon={<AddIcon />}
+          colorScheme="red"
+        >
+          Add Module
+        </Button>
+        <Button
+          onClick={onOpenAddGlobalService}
+          leftIcon={<AddIcon />}
+          colorScheme="yellow"
+        >
+          Add Global Service
+        </Button>
+        <Button
+          onClick={() => generateAngularProject()}
+          leftIcon={<FaDownload />}
+          colorScheme="green"
+          disabled={!isGenerateButtonEnabled()}
+        >
+          Generate Angular Project
+        </Button>
+        {loading && (
+          <Spinner
+            thickness="4px"
+            speed="0.65s"
+            emptyColor="gray.200"
+            color="green.500"
+            size="lg"
+          />
+        )}
       </Stack>
       <br />
-<div className="modules-services-header">
-         {modules.length > 0 && <i>
+      <div className="modules-services-header">
+        {modules.length > 0 && (
+          <i>
             <u>Modules :</u>
-          </i>} 
+          </i>
+        )}
 
-          {globalServices.length > 0 && <i>
+        {globalServices.length > 0 && (
+          <i>
             <u>Global Services :</u>
-          </i>}
-</div>
-     
-
-<div className={modules.length > 0 ? "modules-services-wrapper-gapped" : "modules-services-wrapper"}>
- {/* List Angular Modules */}
- <div>
-        {modules.map((m: IModule) => (
-          <AngularModuleComponent key={m.id} module={m} />
-        ))}
+          </i>
+        )}
       </div>
-       {/* List Angular Global Services */}
-       <div>
-        {globalServices.map((service: IService) => (
-          <div className="angular-service-wrapper">
-          <div className="angular-service-header">
-            <p className="service-name">
-              <FaAngular /> &nbsp;{service.name}
-            </p>
-            <CloseButton
-            onClick={() => removeGlobalService(service.id)}
-              size="md"
-            />
-          </div>
-          </div>
-          
-        ))}
-      </div>
-</div>
-   
 
-{/* Modal to add an angular Module */}
-<Modal isOpen={isOpenAddModule} onClose={onCloseAddModule}>
+      <div
+        className={
+          modules.length > 0
+            ? "modules-services-wrapper-gapped"
+            : "modules-services-wrapper"
+        }
+      >
+        {/* List Angular Modules */}
+        <div>
+          {modules.map((m: IModule) => (
+            <AngularModuleComponent key={m.id} module={m} />
+          ))}
+        </div>
+        {/* List Angular Global Services */}
+        <div>
+          {globalServices.map((service: IService) => (
+            <div className="angular-service-wrapper">
+              <div className="angular-service-header">
+                <p className="service-name">
+                  <FaAngular /> &nbsp;{service.name}
+                </p>
+                <CloseButton
+                  onClick={() => removeGlobalService(service.id)}
+                  size="md"
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      {/* Modal to add an angular Module */}
+      <Modal isOpen={isOpenAddModule} onClose={onCloseAddModule}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Create Module</ModalHeader>
@@ -225,7 +271,6 @@ const AngularScaffolderComponent = () => {
           </ModalFooter>
         </ModalContent>
       </Modal>
-
     </div>
   );
 };
